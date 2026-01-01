842b38d12f187215b9cca4d190fcba40dc8d6e0a
rifat222212-ux <rifat222212@gmail.com>
2026-01-01 11:41:55 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -16,13 +16,13 @@ module.exports = {
 	onStart: async function ({ api, event }) {
 		try {
 			const ownerInfo = {
-				name: '𝑬𝒘𝑹 𝑺𝒉𝑨𝒏',
+				name: '°•✮•° Yᴏᴜʀ Oɴɪᴋ ♡',
 				gender: '𝑴𝒂𝑳𝒆',
-				Birthday: '10-𝟎𝟕-𝟐𝟎𝟎5',
+				Birthday: '17-10-𝟐𝟎𝟎5',
 				religion: '𝙄𝒔𝒍𝑨𝒎',
 				hobby: '𝑺𝒍𝒆𝒆𝑷𝒊𝒏𝑮',
-				Fb: 'https://www.facebook.com/Sh4n.Dev1',
-				Relationship: '𝑺𝒊𝒏𝑮𝒆𝒍',
+				Fb: 'https://www.facebook.com/ Gaming Crush',
+				Relationship: 'Eɴɢᴀɢᴇᴅ ',
 				Height: '5"3'
 			};
 
